3166cbc3211d0cdbc4e589b26d77041f469e9353
sokomishalov <sokolovm@dev.vtb>
2020-03-16 18:15:35 +0300

    cli parsing

--- obfsctr_cli/src/main.rs
+++ obfsctr_cli/src/main.rs
@@ -1,20 +1,22 @@
+use std::fmt::{Display, Formatter, Result};
+
 use clap::Clap;
 
 use obfsctr_core::obfsctr::obfuscate;
 
 #[derive(Clap)]
-#[clap(version = "1.0", author = "Kevin K.")]
+#[clap(version = "0.1.0", author = "sokomishalov")]
 struct Opts {
     /// Sets an input file or directory
     #[clap(short = "i", long = "input")]
     input: String,
 
-    /// Sets an output file or directory
-    #[clap(short = "o", long = "output")]
+    /// Sets an output file or directory (current directory by default)
+    #[clap(short = "o", long = "output", default_value = ".")]
     output: String,
 
-    /// Sets a number of worker threads
-    #[clap(short = "n", long = "n-threads")]
+    /// Sets a number of worker threads (4 by default)
+    #[clap(short = "n", long = "n-threads", default_value = "4")]
     threads: u8,
 
     /// A level of verbosity, and can be used multiple times
@@ -22,11 +24,19 @@ struct Opts {
     verbose: i32,
 }
 
+impl Display for Opts {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "input:{}, output:{} threads:{} verbose:{}", self.input, self.output, self.threads, self.verbose)
+    }
+}
+
 
 fn main() {
     let opts: Opts = Opts::parse();
 
-    let val: String = obfuscate(String::from("todo"));
+    println!("{}", opts);
 
-    println!("{}", val);
+    let obfuscated = obfuscate(String::from("todo"));
+
+    println!("{}", obfuscated);
 }
